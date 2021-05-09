4ec3be25951b6a38c5c664b68cc7265a0359f66e
王嘉俊 <657447542@qq.com>
2021-05-09 10:25:23 +0800

    feat: init project

---
+++ sumk-dubbo-samples/sumk-dubbo-samples-api/src/main/java/org/yx/consumer/impl/ServiceImpl.java
@@ -0,0 +1,15 @@
+package org.yx.consumer.impl;
+
+import org.yx.consumer.DemoService;
+
+/**
+ * @author : wjiajun
+ * @description:
+ */
+public class ServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        return null;
+    }
+}
