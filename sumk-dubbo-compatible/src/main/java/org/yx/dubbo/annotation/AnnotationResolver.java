293477e38b12b54320b856f34617d5049eb4fb4d
王嘉俊 <657447542@qq.com>
2021-06-06 21:36:27 +0800

    feat: 代码调整，去掉小黄点

--- sumk-dubbo-compatible/src/main/java/org/yx/dubbo/annotation/AnnotationResolver.java
+++ sumk-dubbo-compatible/src/main/java/org/yx/dubbo/annotation/AnnotationResolver.java
@@ -1,7 +1,23 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.yx.dubbo.annotation;
 
 
-import org.apache.dubbo.common.utils.Assert;
 import org.apache.dubbo.common.utils.ClassUtils;
 import org.apache.dubbo.common.utils.StringUtils;
 import org.yx.conf.AppInfo;
@@ -77,8 +93,9 @@ public class AnnotationResolver {
 
         }
 
-        Assert.notNull(interfaceClass,
-                "@Service interfaceClass() or interfaceName() or interface class must be present!");
+        if(interfaceClass == null) {
+            throw new IllegalArgumentException("@Service interfaceClass() or interfaceName() or interface class must be present!");
+        }
 
         Asserts.requireTrue(interfaceClass.isInterface(),
                 "The annotated type must be an interface!");
--- sumk-dubbo-compatible/src/main/java/org/yx/dubbo/bean/ReferenceBeanBuilder.java
+++ sumk-dubbo-compatible/src/main/java/org/yx/dubbo/bean/ReferenceBeanBuilder.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.yx.dubbo.bean;
 
 import com.google.common.collect.Iterables;
@@ -14,6 +31,9 @@ import org.yx.util.kit.Asserts;
 
 import java.util.List;
 
+/**
+ * @author wjiajun
+ */
 class ReferenceBeanBuilder extends AnnotatedInterfaceConfigBeanBuilder<ReferenceBean> {
 
     private ReferenceBeanBuilder(DubboBeanSpec dubboBeanSpec) {
@@ -21,7 +41,7 @@ class ReferenceBeanBuilder extends AnnotatedInterfaceConfigBeanBuilder<Reference
     }
 
     private void configureInterface(DubboBeanSpec dubboBeanSpec, ReferenceBean referenceBean) {
-        Boolean generic = dubboBeanSpec.getAnnotationAttributes().getBoolean("generic");
+        boolean generic = dubboBeanSpec.getAnnotationAttributes().getBoolean("generic");
         if (generic) {
             // it's a generic reference
             String interfaceClassName = dubboBeanSpec.getAnnotationAttributes().getString("interfaceName");
